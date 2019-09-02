cd5e4d1fb47af3b9ce379132c1734ddecd4f5a4d
Nitin Prakash <nitin.prakash@nagarro.com>
2019-09-02 16:03:56 +0530

    Finished Creating Services.

---
+++ MyShop/MyShop.Core/ViewModels/BasketSummaryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using MyShop.Services.BasketService;
+
+namespace MyShop.Core.ViewModels
+{
+    public class BasketSummaryViewModel
+    {
+        public int BasketCount { get; set; }
+        public decimal BasketTotal { get; set; }
+
+        public BasketSummaryViewModel()
+        {
+
+        }
+
+        public BasketSummaryViewModel(int BasketCount, decimal BasketTotal)
+        {
+            this.BasketCount = BasketCount;
+            this.BasketTotal = BasketTotal;
+        }
+
+    }
+}
